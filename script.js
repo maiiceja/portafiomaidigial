d9354fa5c85dd8863b0f500d5c49b9671e4dbb04
maiiceja <maiipoynter@gmail.com>
2025-11-07 02:16:31 -0600

    Subiendo mi portafolio inicial

---
+++ script.js
@@ -0,0 +1,10 @@
+const track = document.querySelector(".carousel-track");
+
+track.addEventListener("mouseenter", () => {
+  track.style.animationPlayState = "paused";
+});
+
+track.addEventListener("mouseleave", () => {
+  track.style.animationPlayState = "running";
+});
+
